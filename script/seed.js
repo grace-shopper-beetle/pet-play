668d2f190cc984d864a36afbbaef846d970524a3
rachelj521 <rjiang521@gmail.com>
2022-06-14 16:06:56 -0400

    added users table

--- script/seed.js
+++ script/seed.js
@@ -2,7 +2,116 @@
 
 const {db, models: {User} } = require('../server/db')
 
-const users = 
+const users = [{
+  username: "rsaywell0",
+  password: "qoTNMscWG",
+  email:  "rsaywell0@infoseek.co.jp",
+  first_name:  "Riley",
+  last_name:  "Saywell",
+  street_address:  "10680 Elka Alley",
+  city:  "San Antonio",
+  state:  "TX",
+  zipcode:  "78245",
+  phone:  "8306531237"
+}, {
+  username: "gsigfrid1",
+  password: "bKwXocG",
+  email: "gsigfrid1@scribd.com",
+  first_name: "Gus",
+  last_name: "Sigfrid",
+  street_address: "62 Maywood Circle",
+  city: "Syracuse",
+  state: "NY",
+  zipcode: "13205",
+  phone: "3151028276"
+}, {
+  username: "mrichens2",
+  password: "0SLeNff6kDQm",
+  email: "mrichens2@google.ru",
+  first_name: "Maynord",
+  last_name: "Richens",
+  street_address: "3259 Commercial Place",
+  city: "Tallahassee",
+  state: "FL",
+  zipcode: "32399",
+  phone: "8501565111"
+}, {
+  username: "tcheley3",
+  password: "kOGVvN",
+  email: "tcheley3@seesaa.net",
+  first_name: "Talya",
+  last_name: "Cheley",
+  street_address: "0810 Mayfield Alley",
+  city: "Norfolk",
+  state: "VA",
+  zipcode: "23504",
+  phone: "7572648004"},
+{
+  username: "ddumelow4",
+  password: "KMUz08UjBg72",
+  email: "ddumelow4@instagram.com",
+  first_name: "Dody",
+  last_name: "Dumelow",
+  street_address: "0275 Troy Street",
+  city: "Tampa",
+  state: "FL",
+  zipcode: "33620",
+  phone: "8132641057"
+}, {
+  username: "ssussex5",
+  password: "JducKficr5",
+  email: "ssussex5@tripadvisor.com",
+  first_name: "Skelly",
+  last_name: "Sussex",
+  street_address: "5 Coleman Street",
+  city: "Omaha",
+  state: "NE",
+  zipcode: "68117",
+  phone: "4024334605"
+}, {
+  username: "galcoran6",
+  password: "9WaIVbvpia",
+  email: "galcoran6@reuters.com",
+  first_name: "Ginelle",
+  last_name: "Alcoran",
+  street_address: "74 Vahlen Center",
+  city: "Kansas City",
+  state: "MO",
+  zipcode: "64199",
+  phone: "8164541146"
+}, {
+  username: "apendall7",
+  password: "xW6DMnqBC",
+  email: "apendall7@mit.edu",
+  first_name: "Alika",
+  last_name: "Pendall",
+  street_address: "7 Garrison Point",
+  city: "Harrisburg",
+  state: "PA",zipcode: "17121",
+  phone: "7171126947"
+}, {
+  username: "hdossett8",
+  password: "2FkO1zO7D13",
+  email: "hdossett8@wsj.com",
+  first_name: "Hermie",
+  last_name: "Dossett",
+  street_address: "267 Darwin Pass",
+  city: "Washington",
+  state: "DC",
+  zipcode: "20268",
+  phone: "2021910253"
+}, {
+  username: "mberkely9",
+  password: "WLT0Pn",
+  email: "mberkely9@linkedin.com",
+  first_name: "Minna",
+  last_name: "Berkely",
+  street_address: "46 Oxford Crossing",
+  city: "Pittsburgh",
+  state: "PA",
+  zipcode: "15215",
+  phone: "4126820328"
+}]
 
 const products = [{
   product_name: "Cat Dancer", 
