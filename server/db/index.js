56d28cb8da65095c285b832ad1c16deae956d0a8
Beth Brennan <bethbrennan2@gmail.com>
2022-06-14 15:18:53 -0400

    created User and Product models and associations

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,17 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Product = require('./models/Product')
 
 //associations could go here!
+
+User.belongsToMany(Product, { through: 'Cart' });
+Product.belongsToMany(User, { through: 'Cart' });
 
 module.exports = {
   db,
   models: {
     User,
+    Product
   },
 }
